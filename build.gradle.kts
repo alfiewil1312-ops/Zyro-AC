24ad61c342ecf06b64dd256b399df5c53f8e2d9c
alfiewil1312-ops <alfiewil1312@outlook.com>
2026-08-29 02:40:04 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,21 @@
+plugins {
+    java
+}
 
+group = "io.github.alfiewil1312"
+version = "1.0.0"
+
+repositories {
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
