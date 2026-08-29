d0fbbc3419c250f49e83b84d09230000d8fe5fee
alfiewil1312-ops <alfiewil1312@outlook.com>
2026-08-29 02:42:04 +0100

    Update ZyroAC.java

--- src/main/java/io/github/alfiewil1312/zyroac/ZyroAC.java
+++ src/main/java/io/github/alfiewil1312/zyroac/ZyroAC.java
@@ -1,1 +1,16 @@
+package io.github.alfiewil1312.zyroac;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ZyroAC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("ZyroAC enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("ZyroAC disabled!");
+    }
+}
